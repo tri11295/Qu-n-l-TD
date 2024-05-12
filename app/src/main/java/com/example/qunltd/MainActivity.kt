9e7a8b65238b0e51ed15615f80f98164ea956a8a
tri112 <quangtri110295@gmail.com>
2024-05-12 19:59:33 +0700

    add config

--- app/src/main/java/com/example/qunltd/MainActivity.kt
+++ app/src/main/java/com/example/qunltd/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.qunltd
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.qunltd.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding : MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = DataBindingUtil.setContentView(this,R.layout.main_activity)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
---
+++ app/src/main/java/com/example/qunltd/ui/home/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.qunltd.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.qunltd.R
+import com.example.qunltd.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+
+        }
+    }
+}
